5a25135ec99be225984c788f31044dd76f2d96a0
vyrwu <dev.anowak@gmail.com>
2023-09-24 16:28:13 +0200

    feat: format on save

---
+++ lua/custom/configs/null-ls.lua
@@ -0,0 +1,28 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+  b.formatting.stylua,
+  b.formatting.prettierd,
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
+  b.formatting.terraform_fmt,
+  b.formatting.terrafmt,
+  b.formatting.textlint,
+  b.formatting.trim_newlines,
+  b.formatting.trim_whitespace,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = function()
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
+}
